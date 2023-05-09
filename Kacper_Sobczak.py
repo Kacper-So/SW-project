2e6efd3dd1a29ed3f21e8a91750135891515fa4f
Kacper Sobczak <66113631+Kacper-So@users.noreply.github.com>
2023-05-09 13:07:45 +0200

    + naive solution

--- Kacper_Sobczak.py
+++ Kacper_Sobczak.py
@@ -8,6 +8,29 @@ from processing.utils import perform_processing
 
 
 def main():
+
+    #Stworzenie słonika z możliwymi znakami oraz ich obrazami
+    characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z', 'V', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    img = cv2.imread('font.png')
+
+    height, width, channels = img.shape
+    char_spaceing = 25
+    part_height = height // 5
+    part_width = (width - (7 * char_spaceing)) // 8
+    char_img_parts = []
+    for i in range(5):
+        incremental_char_spaceing = 0
+        for j in range(8):
+            part = img[i * part_height:(i + 1) * part_height, incremental_char_spaceing + (j * part_width):incremental_char_spaceing + ((j + 1) * part_width)]
+            char_img_parts.append(part)
+            incremental_char_spaceing = incremental_char_spaceing + char_spaceing
+
+    characters_dict = {}
+    for i in range(len(characters)):
+        characters_dict[characters[i]] = char_img_parts[i]
+
+
+
     parser = argparse.ArgumentParser()
     parser.add_argument('images_dir', type=str)
     parser.add_argument('results_file', type=str)
@@ -24,7 +47,7 @@ def main():
             print(f'Error loading image {image_path}')
             continue
 
-        results[image_path.name] = perform_processing(image)
+        results[image_path.name] = perform_processing(image, characters_dict)
 
     with results_file.open('w') as output_file:
         json.dump(results, output_file, indent=4)
--- processing/utils.py
+++ processing/utils.py
@@ -1,28 +1,49 @@
 import numpy as np
 import cv2
+import imutils
 
-def perform_processing(image: np.ndarray) -> str:
+def perform_processing(image: np.ndarray, character_dict: dict) -> str:
     print(f'image.shape: {image.shape}')
     # TODO: add image processing here
 
-    #Stworzenie słonika z możliwymi znakami oraz ich obrazami
-    characters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z', 'V', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    img = cv2.imread('font.png')
+    # Ograniczenie obszaru, gdzie znajduje się tablica
+    img = image
+    img = cv2.resize(img, (800,600))
+    img_og = img.copy()
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    kernel = np.ones((2, 2), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    cont, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    img_cont = img_og.copy()
+    cv2.drawContours(img_cont, cont, -1, (0,255,0), 3)
+    # cv2.imshow("test", img_cont)
+    max_contour = max(cont, key=cv2.contourArea)
+    x, y, w, h = cv2.boundingRect(max_contour)
+    plate = img_og[y:y+h, x:x+w]
+    plate = cv2.resize(plate, (900, 600))
+    # cv2.imshow("test2", plate)
+    # cv2.waitKey(0)
 
-    height, width, channels = img.shape
-    char_spaceing = 25
-    part_height = height // 5
-    part_width = (width - (7 * char_spaceing)) // 8
-    char_img_parts = []
-    for i in range(5):
-        incremental_char_spaceing = 0
-        for j in range(8):
-            part = img[i * part_height:(i + 1) * part_height, incremental_char_spaceing + (j * part_width):incremental_char_spaceing + ((j + 1) * part_width)]
-            char_img_parts.append(part)
-            incremental_char_spaceing = incremental_char_spaceing + char_spaceing
-
-    characters_dict = {}
-    for i in range(len(characters)):
-        characters_dict[characters[i]] = char_img_parts[i]
+    #reiteracja
+    img = plate
+    img = cv2.resize(img, (800,600))
+    img_og = img.copy()
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((2, 2), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    cont, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    img_cont = img_og.copy()
+    cv2.drawContours(img_cont, cont, -1, (0,255,0), 3)
+    cv2.imshow("test", img_cont)
+    max_contour = max(cont, key=cv2.contourArea)
+    x, y, w, h = cv2.boundingRect(max_contour)
+    plate = img_og[y:y+h, x:x+w]
+    plate = cv2.resize(plate, (900, 600))
+    cv2.imshow("test2", plate)
+    cv2.waitKey(0)
 
     return 'PO12345'
